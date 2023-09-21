c9882331afecbec33760d64b5a805c1d18d18e34
Sergej <129406138+Tumba123@users.noreply.github.com>
2023-09-21 11:06:46 +0200

    Update

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ let recognition = null;
 const numbersList = [5, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 25, 26, 27, 28, 29, 30, 31, 33, 34, 35, 71, 83, 107, 110, 111, 115, 122, 123, 124, 125, 126];
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
 
-document.getElementById('start-recognition').addEventListener('click', () => {
+let isListening = false; // Track the listening state
+
+// Create a function to toggle recognition on/off
+function toggleRecognition() {
     if (!recognition) {
         recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
         recognition.lang = 'en-US'; // Set the language (you can change it)
@@ -11,25 +14,30 @@ document.getElementById('start-recognition').addEventListener('click', () => {
             const result = event.results[0][0].transcript;
             document.getElementById('output').innerText = 'Result received: ' + result;
             markNumberAsChecked(result);
-            // Continue recognition after a short delay to capture the next result
-            setTimeout(() => {
+        };
+
+        recognition.onend = () => {
+            if (isListening) {
+                // If still in listening state, restart recognition
                 recognition.start();
-            }, 100);
+            }
         };
     }
 
-    document.getElementById('start-recognition').style.display = 'none';
-    document.getElementById('stop-recognition').style.display = 'block';
-    recognition.start();
-});
-
-document.getElementById('stop-recognition').addEventListener('click', () => {
-    if (recognition) {
+    if (!isListening) {
+        recognition.start();
+        isListening = true;
+    } else {
         recognition.stop();
+        isListening = false;
     }
-    document.getElementById('start-recognition').style.display = 'block';
-    document.getElementById('stop-recognition').style.display = 'none';
-});
+
+    // Toggle the button text
+    const startButton = document.getElementById('start-recognition');
+    startButton.innerText = isListening ? 'Stop Recognition' : 'Start Recognition';
+}
+
+document.getElementById('start-recognition').addEventListener('click', toggleRecognition);
 
 function markNumberAsChecked(text) {
     // Use a regular expression to search for numbers in the text
