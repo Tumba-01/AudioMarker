3c1964d15ef2c24e289c9c680f521eec3d22a7cd
Sergej <129406138+Tumba123@users.noreply.github.com>
2023-09-22 10:21:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -123,29 +123,44 @@ window.addEventListener('load', openWorkaroundModal);
 // Close the modal when the "OK" button is clicked
 document.getElementById('workaround-modal-button').addEventListener('click', closeWorkaroundModal);
 
-var Util={};
-Util.base64 = function(mimeType, base64) {
-  return 'data:' + mimeType + ';base64,' + base64;
-};
+let wakeLock = null; // Declare a variable to store the wake lock object
 
-var Util = {};
+// Function to request the wake lock
+async function requestWakeLock() {
+  // Check if the Wake Lock API is available in the browser
+  if ('wakeLock' in navigator) {
+    try {
+      // Request a wake lock
+      wakeLock = await navigator.wakeLock.request('screen');
 
-Util.base64 = function (mimeType, base64) {
-  return 'data:' + mimeType + ';base64,' + base64;
-};
-
-var video = document.createElement('video');
-video.setAttribute('loop', '');
-
-function addSourceToVideo(element, type, dataURI) {
-  var source = document.createElement('source');
-  source.src = dataURI;
-  source.type = 'video/' + type;
-  element.appendChild(source);
+      // The screen won't dim while the wake lock is active
+    } catch (err) {
+      console.error('Could not request wake lock:', err);
+    }
+  }
 }
 
-addSourceToVideo(video, 'webm', Util.base64('video/webm', 'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
-addSourceToVideo(video, 'mp4', Util.base64('video/mp4', 'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAABzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+// Function to release the wake lock
+function releaseWakeLock() {
+  if (wakeLock !== null) {
+    wakeLock.release();
+    wakeLock = null;
+  }
+}
 
-video.play();
+// Example of using the wake lock:
+document.getElementById('start-recording').addEventListener('click', () => {
+  // Request the wake lock when starting recording
+  requestWakeLock();
+
+  // Start recording logic
+});
+
+document.getElementById('stop-recording').addEventListener('click', () => {
+  // Stop recording logic
+
+  // Release the wake lock when recording is stopped
+  releaseWakeLock();
+});
+
 
